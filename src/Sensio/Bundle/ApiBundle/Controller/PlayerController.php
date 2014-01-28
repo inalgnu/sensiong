3d804b68c0699a83af203e1b03a749e5cf78fd9d
inalgnu <inal.djafar@sensiolabs.com>
2014-01-29 00:12:44 +0100

    add crud rest controllers

--- src/Sensio/Bundle/ApiBundle/Controller/PlayerController.php
+++ src/Sensio/Bundle/ApiBundle/Controller/PlayerController.php
@@ -3,14 +3,42 @@
 namespace Sensio\Bundle\ApiBundle\Controller;
 
 use FOS\RestBundle\Controller\Annotations as Rest;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Sensio\Bundle\ApiBundle\Entity\Player;
 
-class PlayerController
+class PlayerController extends Controller
 {
     /**
      * @Rest\View()
      */
     public function queryAction()
     {
-        return array();
+        return $this->getManager('Player')->findAll();
+    }
+
+    /**
+     * @RestView()
+     */
+    public function getAction(Player $player)
+    {
+        return $player;
+    }
+
+    /**
+     * @ParamConverter("player", converter="fos_rest.request_body")
+     * @Rest\View(statusCode=201)
+     */
+    public function newAction(Player $player)
+    {
+        return $this->save($player);
+    }
+
+    /**
+     * @ParamConverter("requestPlayer", converter="fos_rest.request_body")
+     * @Rest\View(statusCode=204)
+     */
+    public function updateAction(Player $player, Player $requestPlayer)
+    {
+        $this->save($player->updateFrom($requestPlayer));
     }
 }
